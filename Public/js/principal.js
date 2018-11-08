5f776cbd80b1700a0b3eb7f975cdb4fb076b2df1
Your Name <you@example.com>
2018-11-08 11:56:18 -0500

    2 commit

--- Public/js/principal.js
+++ Public/js/principal.js
@@ -1,7 +1,7 @@
 var config = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   parent: 'phaser-example',
-  width: 800,
+  width: 1000,
   height: 600,
   physics: {
     default: 'arcade',
@@ -20,17 +20,28 @@ var config = {
 var juego = new Phaser.Game(config);
  
 function preload() {
-    //cargar recursos
-      juego.stage.backgroundColor="#C29A91";
+  this.load.image('bg','images/bg.jpg')
+  this.load.image('box','images/box.jpg')      
 }
- 
+var counter = 30;
 function create() {
-    //
-}
- 
-function update() {
-    
-}
+  var self= this;
+  this.add.tileSprite(300,20,500,1000,'bg');
+  this.add.tileSprite(920,0,300,1500,'box');
+  this.socket = io();
+  this.add.text(16, 530, 'Jugador 1', { fontSize: '32px', fill: '#0000FF' });
+  this.add.text(570, 530, 'Jugador 2', { fontSize: '32px', fill: '#FF0000' });
+  text = this.add.text(800, 16, 'Time 00:'+counter, { font: "32px Arial", fill: "#ffffff", align: "center" });
+  timer = juego.time.create(false);
 
-juego.state.add('principal',scene);
-juego.state.start('principal');
+
+    timer.loop(2000, updateCounter, this);
+
+    timer.start();
+}
+function update() {
+ }
+function updateCounter() {
+  counter++;
+  text.setText('Time 00:' + counter);
+}

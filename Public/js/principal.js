e53755b14e4966d541f50e687150afb6c50977e3
Manlio Tejeda <manliot@uninorte.edu.co>
2018-11-11 01:03:54 -0500

    Actualizacion 11/11/2018 1:03 se trabajo en tuberias

--- Public/js/principal.js
+++ Public/js/principal.js
@@ -29,7 +29,8 @@ var inpuntClick;
 var text;
 var clr, cud, cd1, cd2, cd3, cd4;//aqui vamos a guardar el txt de las capacidades para poder moverlo
 var groupTank;
-
+var sw = false;//esta variable se activa si se esta moviendo una tuberia
+var cursorKeys;
 function preload() {
   this.load.image('bg', 'images/bg.jpg')
   this.load.image('UD', 'images/UD.png')
@@ -45,20 +46,23 @@ function preload() {
 function create() {
 
   cargaInicial(this);
-  moverSprite(lr, this, clr);
-  moverSprite(ud, this, cud);
-  moverSprite(d1, this, d1);
-  moverSprite(d2, this, d2);
-  moverSprite(d3, this, d3);
-  moverSprite(d4, this, d4);
+  moverSprite(lr, this, "LR");
+  moverSprite(ud, this, "UD");
+  moverSprite(d1, this, "D1");
+  moverSprite(d2, this, "D2");
+  moverSprite(d3, this, "D3");
+  moverSprite(d4, this, "D4");
 }
 function update() {
-  this.physics.arcade.collide(groupTank,lr);
+  console.log("hola")
+  //this.physics.arcade.collide(groupTank,lr);
   if (tl != null) {
 
     tl.on('pointerdown', function (pointer) {//se activa cuandola tuberia se le esta haciendo click
-
-
+      sw = true;
+      /*if(cursorKeys.BACKSPACE.isDown){
+        tl.angle=90;
+      }*/
       tl.on('drag', function (pointer, dragX, dragY) {//esta funcion se activa caundo hay un arrastre en el objeto tl
         tl.x = dragX;
         tl.y = dragY;
@@ -70,6 +74,7 @@ function update() {
       //comproamos que la tuberia concuerde con otra y si no es asi entonces la destruimos
       if (correct()) {
         tl.destroy();
+        sw = false;
       }
     });
   } else {
@@ -88,40 +93,56 @@ function moverSprite(algo, ga, t) {//recibe un sprite
   algo.on('pointerout', function (pointer) {
     console.log("se esta paso el mause por ahi");
 
-    if (pointer.isDown) {
+    if (pointer.isDown & !sw) {
       tl = null;
       txte = null;
-      tl = ga.add.sprite(pointer.x, pointer.y, 'LR').setInteractive({ draggable: true });
+      tl = ga.add.sprite(pointer.x, pointer.y, t).setInteractive({ draggable: true });
       //falta mover el texto de la capacidad
+      sw = false;
     }
   });
 }
 
 function cargaInicial(game) {
 
+
+
   var counter = 30;
   game.add.tileSprite(0, 150, 2000, 1500, 'bg');
-  lr = game.add.sprite(890, 100, 'LR').setInteractive();
+  //el .setInteractive sirve para poder mover con el rator
+  lr = game.physics.add.sprite(890, 100, 'LR').setInteractive();
   ud = game.add.sprite(890, 180, 'UD').setInteractive();
   d1 = game.add.sprite(890, 260, 'D1').setInteractive();
   d2 = game.add.sprite(890, 360, 'D2').setInteractive();
   d3 = game.add.sprite(890, 440, 'D3').setInteractive();
   d4 = game.add.sprite(890, 520, 'D4').setInteractive();
-  groupTank = game.add.group();
-  groupTank.enableBody=true;//esto le dice que los objetos de esta madre tiene cuerpo (algo cleve en las colisiones)
 
+  //se crea u grupo de tanques y le asignamos un cuerpo
+  groupTank = game.physics.add.group();
+  groupTank.enableBody = true;//esto le dice que los objetos de esta madre tiene cuerpo (algo cleve en las colisiones)
+
+  //aqui agregamos los tanks a el grupo de tanques
   tank1 = groupTank.create(pos[Math.floor(Math.random() * 5)], 50, 'Tank');
   tank2 = groupTank.create(pos[Math.floor(Math.random() * 5)], 450, 'Tank');
-  
+
+  //las guardamos en variables globales para luego poder utilizarlas al momento de construir el grafo
   clr = game.add.text(880, 93, '' + capacidades[Math.floor(Math.random() * 5)], { fontSize: '16px', fill: '#0000FF' });
   cud = game.add.text(880, 173, '' + capacidades[Math.floor(Math.random() * 5)], { fontSize: '16px', fill: '#0000FF' });
   cd1 = game.add.text(873, 263, '' + capacidades[Math.floor(Math.random() * 5)], { fontSize: '16px', fill: '#0000FF' });
   cd2 = game.add.text(873, 343, '' + capacidades[Math.floor(Math.random() * 5)], { fontSize: '16px', fill: '#0000FF' });
   cd3 = game.add.text(885, 423, '' + capacidades[Math.floor(Math.random() * 5)], { fontSize: '16px', fill: '#0000FF' });
   cd4 = game.add.text(885, 523, '' + capacidades[Math.floor(Math.random() * 5)], { fontSize: '16px', fill: '#0000FF' });
+
   game.socket = io();
+
+  groupTank.enableBody = true;
+  groupTank.physicsBodyType = Phaser.Physics.ARCADE;
+
+  //texto de los jugadores
   game.add.text(16, 530, 'Jugador 1', { fontSize: '32px', fill: '#0000FF' });
   game.add.text(570, 530, 'Jugador 2', { fontSize: '32px', fill: '#FF0000' });
   //text = this.add.text(800, 16, 'Time 00:' + counter, { font: "32px Arial", fill: "#ffffff", align: "center" });
-  
+
+  //this.physics.arcade.overlap(tl, groupTank);
+
 }

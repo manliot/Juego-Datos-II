afb2c2a5787a2344159561079210083dd3b72934
Manlio Tejeda <manliot@uninorte.edu.co>
2018-11-14 16:55:23 -0500

    Actualizacion 14/11/18 4:55

--- Public/js/principal.js
+++ Public/js/principal.js
@@ -18,19 +18,21 @@ var config = {
 };
 
 var juego = new Phaser.Game(config);
-
+var sl, su, s1, s2, s3, s4;
+var swsocket = false;
+var temp
 var lr, ud;
 var d1, d2, d3, d4;
 var tank1, tank2;
-var prueba;
+
 var tl = null;//aqui se guarda el sprite que se va a mover
+var tlText;
 var txte;// esta es la capacidad de el sprite que se va a mover
-var inpuntClick;
-var text;
+
 var clr, cud, cd1, cd2, cd3, cd4;//aqui vamos a guardar el txt de las capacidades para poder moverlo
 var groupTank;
 var sw = false;//esta variable se activa si se esta moviendo una tuberia
-var AKey, SKey;
+
 function preload() {
   this.load.image('bg', 'images/bg.jpg')
   this.load.image('UD', 'images/UD.png')
@@ -46,70 +48,113 @@ function preload() {
 function create() {
 
   cargaInicial(this);
-  moverSprite(lr, this, "LR");
-  moverSprite(ud, this, "UD");
-  moverSprite(d1, this, "D1");
-  moverSprite(d2, this, "D2");
-  moverSprite(d3, this, "D3");
-  moverSprite(d4, this, "D4");
+
+  moverSprite(lr, this, "LR", sl);
+  moverSprite(ud, this, "UD", su);
+  moverSprite(d1, this, "D1", s2);
+  moverSprite(d2, this, "D2", s2);
+  moverSprite(d3, this, "D3", s3);
+  moverSprite(d4, this, "D4", s4);
+
 }
+var tuberiaotro, Imagetu;
 function update() {
 
-
+  //this.physics.overlap(tank1,tl,conectar(tank1,tl),null,this);
   if (tl != null) {
 
     tl.on('pointerdown', function (pointer) {//se activa cuandola tuberia se le esta haciendo click
       sw = true;
 
       tl.on('drag', function (pointer, dragX, dragY) {//esta funcion se activa caundo hay un arrastre en el objeto tl
-        //console.log(tl.angle);
 
-        tl.x = dragX;
-        tl.y = dragY;
+        tl.x = tlText.x = dragX;
+        tl.y = tlText.y = dragY;
 
       });
 
     });
+    this.socket.emit("memovi", tl);
 
-    tl.on('pointerup', function (pointer) {
+    tl.on('pointerout', function (pointer) {
       //comproamos que la tuberia concuerde con otra y si no es asi entonces la destruimos
-      if (correct()) {
+      if (!correct(tl)) {
         tl.destroy();
-        sw = false;
+
       }
+      sw = false;
     });
-    this.socket.emit('seMovio', tl, tl.x, tl.y, tl.angle);
+
   } else {
     //console.log("null(no te awites!)");
   }
+  this.socket.on('otroMovio', function (tube, ID, pls) {
+
+    console.log(ID);
+
+    if (IDe == ID) {
+      console.log("songiuales");
+    } else {
+      tuberiaotro = tl = tub = G.add.sprite(tube.x, tube.y, Imagetu).setInteractive({ draggable: true });
+
+    }
+
+  });
 
 }
+function conectar(tank, t) {
 
-function correct() {
+}
+function correct(tl) {
   //aqui se comprueba si la tuberia conecta a otra tuberia
-  return true;
+
+
+  if (temp != null) {
+    /*console.log(temp.x);
+    console.log(temp.y);
+    console.log(tl.x);
+    console.log(tl.y);*/
+    if (temp.x + 50 > tl.x & temp.x - 50 < tl.x & temp.y + 50 > tl.y & temp.y - 50 < tl.y) {
+
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return true;
+  }
 }
 
 
 
-
-function moverSprite(algo, ga, t) {//recibe un sprite
+function moverSprite(algo, ga, t, c) {//recibe un sprite
   algo.on('pointerout', function (pointer) {//se activa cuando se pasa eñ ratpn por  el sprite algo 
-
+    Imagetu = t;
     if (pointer.isDown & !sw) {
       if (tl != null) {
-        tl.destroy();
+        if (tl.x > 760) {
+
+          tl.destroy();
+        }
+        temp = tl;
       }
       tl = null;//ESTO PARA QUE CUADO SE ESTE MOVIENDO OTRA TUERIA NO SEA LA MIMA QUE L ANTERIOR
       txte = null;
-      tl = ga.add.sprite(pointer.x, pointer.y, t).setInteractive({ draggable: true });
-      //falta mover el texto de la capacidad
+      tl = tub = ga.add.sprite(pointer.x, pointer.y, t).setInteractive({ draggable: true });
+
+
+      tlText = ga.add.text(pointer.x, pointer.y, '' + c, { fontSize: '16px', fill: '#0000FF' });
+
       sw = false;
     }
   });
 }
-
+var IDe, G;
 function cargaInicial(game) {
+  G = game;
+  game.socket = io();
+  self = this;
+  //this.otherPlayers = this.physics.add.group();
 
   game.add.tileSprite(0, 150, 2000, 1500, 'bg');
   //el .setInteractive sirve para poder mover con el rator
@@ -120,50 +165,35 @@ function cargaInicial(game) {
   d3 = game.add.sprite(890, 440, 'D3').setInteractive();
   d4 = game.add.sprite(890, 520, 'D4').setInteractive();
 
-
   //se crea u grupo de tanques y le asignamos un cuerpo
   groupTank = game.physics.add.group();
   groupTank.enableBody = true;//esto le dice que los objetos de esta madre tiene cuerpo (algo cleve en las colisiones)
 
-  game.socket = io();
-
-  var s1, s2, s3, s4, st1, st2, sl, su;
   game.socket.on('Capacidades', function (c1, c2, c3, c4, ct1, ct2, cl, cu) {
+    swsocket = true;
+    tank1.x = ct1; tank2.x = ct2; clr.setText(cl); cud.setText(cu);
+    cd1.setText(c1); cd2.setText(c2); cd3.setText(c3); cd4.setText(c4);
+
     s1 = c1; s2 = c2; s3 = c3; s4 = c4; sl = cl; su = cu; st1 = ct1; st2 = ct2;
-
-    //aqui agregamos los tanks a el grupo de tanques
-    tank1 = groupTank.create(st1, 50, 'Tank');
-    tank2 = groupTank.create(st2, 450, 'Tank');
-
-    //las guardamos en variables globales para luego poder utilizarlas al momento de construir el grafo
-    clr = game.add.text(880, 93, '' + sl, { fontSize: '16px', fill: '#0000FF' });
-    cud = game.add.text(880, 173, '' + su, { fontSize: '16px', fill: '#0000FF' });
-    cd1 = game.add.text(873, 263, '' + s1, { fontSize: '16px', fill: '#0000FF' });
-    cd2 = game.add.text(873, 343, '' + s2, { fontSize: '16px', fill: '#0000FF' });
-    cd3 = game.add.text(885, 423, '' + s3, { fontSize: '16px', fill: '#0000FF' });
-    cd4 = game.add.text(885, 523, '' + s4, { fontSize: '16px', fill: '#0000FF' });
+    console.log(game.socket.id);
+    IDe = game.socket.id;
 
   })
 
-  /*game.socket.on('currentPlayers', function (players) {
-    Object.keys(players).forEach(function (id) {
-      if (players[id].playerId === self.socket.id) {
-        addPlayer(self, players[id]);
-      } else {
-        addOtherPlayers(self, players[id]);
-      }
-    });
-  });*/
-  game.socket.on('newPlayer', function (playerInfo) {
-    addOtherPlayers(self, playerInfo);
-  });
-  game.socket.on('disconnect', function (playerId) {
-    /* self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-    if (playerId === otherPlayer.playerId) {
-      otherPlayer.destroy();
-    }
-  });*/
-  });
+
+  //aqui agregamos los tanks a el grupo de tanques
+  tank1 = groupTank.create(0, 60, 'Tank');
+  tank2 = groupTank.create(0, 438, 'Tank');
+
+  //las guardamos en variables globales para luego poder utilizarlas al momento de construir el grafo
+  clr = game.add.text(880, 93, '' + null, { fontSize: '16px', fill: '#0000FF' });
+  cud = game.add.text(880, 173, '' + null, { fontSize: '16px', fill: '#0000FF' });
+  cd1 = game.add.text(873, 263, '' + null, { fontSize: '16px', fill: '#0000FF' });
+  cd2 = game.add.text(873, 343, '' + null, { fontSize: '16px', fill: '#0000FF' });
+  cd3 = game.add.text(885, 423, '' + null, { fontSize: '16px', fill: '#0000FF' });
+  cd4 = game.add.text(885, 523, '' + null, { fontSize: '16px', fill: '#0000FF' });
+
+
   groupTank.enableBody = true;
   groupTank.physicsBodyType = Phaser.Physics.ARCADE;
 
@@ -173,18 +203,8 @@ function cargaInicial(game) {
   //text = this.add.text(800, 16, 'Time 00:' + counter, { font: "32px Arial", fill: "#ffffff", align: "center" });
 
   ;
-  AKey = game.input.keyboard.addKey('A');
-  SKey = game.input.keyboard.addKey('S');
 
-  game.socket.on('seMovio', function (playerInfo) {
-    /*tl.destroy();
-    sw = false;
-    tl = playerInfo.tlr;
-    tl.x = playerInfo.x;
-    tl.y = playerInfo.y;
-    tl.angle = playerInfo.a;*/
-  });
+
 }
-function addPlayer(self, playerInfo) {
-  ///alll
-}
+
+
--- server.js
+++ server.js
@@ -3,56 +3,63 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var players = {};
-var capacidades = [10, 20, 30, 40, 50], pos = [100, 250, 400, 550, 700];
-var pt1 = pos[Math.floor(Math.random() * 5)], pt2 = pos[Math.floor(Math.random() * 5)];
-var kl = capacidades[Math.floor(Math.random() * 5)], ku = capacidades[Math.floor(Math.random() * 5)], k1 = capacidades[Math.floor(Math.random() * 5)], k2 = capacidades[Math.floor(Math.random() * 5)], k3 = capacidades[Math.floor(Math.random() * 5)], k4 = capacidades[Math.floor(Math.random() * 5)];
 app.use(express.static(__dirname + '/public'));
+var capacidades = [10, 20, 30, 40, 50], pos = [100, 250, 400, 550, 700];
+var pt1, pt2;
+var kl, ku, k1, k2, k3, k4;
+var tub1, tub2;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected' + socket.id);
-  socket.emit("Capacidades", k1, k2, k3, k4, pt1, pt2, kl, ku);
+  console.log('<<<<<<a user connected >>>>>: ' + socket.id);
+
+  actualizarVariables();
+
+  io.sockets.emit("Capacidades", k1, k2, k3, k4, pt1, pt2, kl, ku);
 
   // create a new player and add it to our players object
   players[socket.id] = {
     playerId: socket.id,
-    // tlr, x, y, a
+    tuberiam: null,
   };
-  socket.on('seMovio', function (tlt, x, y, a) {
-    players[socket.id].tlr = tlt;
-    players[socket.id].x = x;
-    players[socket.id].y = y;
-    players[socket.id].a = a;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('seMovio', players[socket.id]);
+
+  socket.on('memovi', function (turu) {
+    players[socket.id].tuberiam = turu;
+    
+    socket.broadcast.emit('otroMovio', players[socket.id].tuberiam, socket.id);
   })
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
 
   socket.on('disconnect', function () {
+    console.log('user disconnected: ', socket.id);
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
 
-    console.log('user disconnected');
   });
+
 });
 
 server.listen(3000, function () {
+  console.log("servidor iniciado")
   console.log(`Listening on ${server.address().port}`);
+
 });
 
 
 
 function addOtherPlayers(self, playerInfo) {
-  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-  if (playerInfo.team === 'blue') {
-    otherPlayer.setTint(0x0000ff);
-  } else {
-    otherPlayer.setTint(0xff0000);
-  }
-  otherPlayer.playerId = playerInfo.playerId;
-  self.otherPlayers.add(otherPlayer);
+
+}
+function actualizarVariables() {
+  capacidades = [10, 20, 30, 40, 50], pos = [100, 250, 400, 550, 700];
+  pt1 = pos[Math.floor(Math.random() * 5)], pt2 = pos[Math.floor(Math.random() * 5)];
+  kl = capacidades[Math.floor(Math.random() * 5)], ku = capacidades[Math.floor(Math.random() * 5)],
+    k1 = capacidades[Math.floor(Math.random() * 5)],
+    k2 = capacidades[Math.floor(Math.random() * 5)],
+    k3 = capacidades[Math.floor(Math.random() * 5)],
+    k4 = capacidades[Math.floor(Math.random() * 5)];
+
 }

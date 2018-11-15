a26462e2e1e66efb5887320e76f5322286d8966f
Manlio Tejeda <manliot@uninorte.edu.co>
2018-11-14 23:25:52 -0500

    actualizacion 14/11/18 11:25

--- Public/js/principal.js
+++ Public/js/principal.js
@@ -28,7 +28,7 @@ var tank1, tank2;
 var tl;//aqui se guarda el sprite que se va a mover
 var tlText;
 var txte;// esta es la capacidad de el sprite que se va a mover
-
+var winn=false;
 var clr, cud, cd1, cd2, cd3, cd4;//aqui vamos a guardar el txt de las capacidades para poder moverlo
 var groupTank;
 var sw = false;//esta variable se activa si se esta moviendo una tuberia
@@ -41,38 +41,38 @@ function preload() {
   this.load.image('D2', 'images/D2.png')
   this.load.image('D3', 'images/D3.png')
   this.load.image('D4', 'images/D4.png')
+  this.load.image('L', 'images/lose.jpg')
+  this.load.image('W', 'images/won.jpg')
   this.load.image('Tank', 'images/Tanque.png')
-  this.load.image('perdio', 'images/perder.png')
-  this.load.image('gano', 'images/ganar.png')
 }
 
-var tiempo2, counter = 0;
+var tiempo2,counter=0;
 function create() {
 
   cargaInicial(this);
-  temp = tank1;
+  temp=tank1;
   tiempo2 = this.add.text(850, 50, 'Time 00:' + counter, { fontSize: '16px', fill: '#FFFFFF' });
-  moverSprite(lr, this, "LR", sl);
-  moverSprite(ud, this, "UD", su);
-  moverSprite(d1, this, "D1", s2);
-  moverSprite(d2, this, "D2", s2);
-  moverSprite(d3, this, "D3", s3);
-  moverSprite(d4, this, "D4", s4);
+  moverSprite(lr, this, "LR");
+  moverSprite(ud, this, "UD");
+  moverSprite(d1, this, "D1");
+  moverSprite(d2, this, "D2");
+  moverSprite(d3, this, "D3");
+  moverSprite(d4, this, "D4");
 
 }
 var tuberiaotro, Imagetu;
-var per;
 function update() {
-
-  if (counter / 50 > -31) {
+  if(winn){var wi  = this.add.sprite(500, 300, 'W')}
+  
+  if(counter/50>-61&!winn){
     counter--;
-  } else {
-    per = this.add.sprite(500, 300, 'perdio');
-  }
-  if (counter / 50 < 10) {
-    tiempo2.setText('Time ' + Math.trunc((counter / 50) + 31));
-  } else {
-    tiempo2.setText('Time 0' + Math.trunc((counter / 50) + 31));
+  if(counter/50<10){
+    tiempo2.setText('Time ' + Math.trunc((counter / 50)+61));
+  }}
+  if(counter/50==-61){
+
+    var ls = this.add.sprite(500, 300, 'L')
+    
   }
   var self = this;
   //this.physics.overlap(tank1,tl,conectar(tank1,tl),null,this);
@@ -84,7 +84,7 @@ function update() {
       tl.on('drag', function (pointer, dragX, dragY) {//esta funcion se activa caundo hay un arrastre en el objeto tl
 
         tl.x = dragX;
-        tl.y  = dragY;
+        tl.y = dragY;
 
       });
 
@@ -96,11 +96,11 @@ function update() {
       //comproamos que la tuberia concuerde con otra y si no es asi entonces la destruimos
       if (!correct(tl)) {
         tl.destroy();
-       // tlText.destroy();
-      } else
-        if (conect2ndtank(tl)) {
-          this.add.image(500, 300, 'gano');
-        }
+        //tlText.destroy();
+      }else
+      if(conect2ndtank(tl)){
+        winn = true;
+      }
       sw = false;
     });
     self.socket.emit("memovi", tl, Imagetu);
@@ -110,25 +110,24 @@ function update() {
 
 
 }
-function gamerover(ga) {
+function gamerover(){
   console.log('Juego finalizado');
-  if (conect2ndtank(tl)) {
-    ga.add.sprite(500, 300, 'gano');
-  } else {
-    ga.add.sprite(500, 300, 'perdio');
+  if(win()){
+
+  }else{
+
   }
 }
 function conect2ndtank(tl) {
-
-  if (tl.x + 53 > tank2.x & tl.x - 47 < tank2.x & tl.y + 55 > tank2.y) {
-
+  
+  if(tl.x + 53 > tank2.x & tl.x - 47 < tank2.x & tl.y + 55 > tank2.y ){
+  
     return true;
 
-  } else {
-
-    return false
-  }
-
+  }else{
+  
+    return false}
+  
 }
 function correct(tl) {
   //aqui se comprueba si la tuberia conecta a otra tuberia
@@ -139,15 +138,15 @@ function correct(tl) {
     console.log(temp.y);
     console.log(tl.x);
     console.log(tl.y);*/
-    if (temp == tank1) {
+    if(temp==tank1){
       if (temp.x + 53 > tl.x & temp.x - 47 < tl.x & temp.y + 57 > tl.y & temp.y - 52 < tl.y) {
 
         return true;
-
+  
       } else {
         return false;
       }
-    } else if (temp.x + 53 > tl.x & temp.x - 47 < tl.x & temp.y + 49 > tl.y & temp.y - 52 < tl.y) {
+    }else if (temp.x + 53 > tl.x & temp.x - 47 < tl.x & temp.y + 49 > tl.y & temp.y - 52 < tl.y) {
 
       return true;
 
@@ -160,7 +159,7 @@ function correct(tl) {
 }
 
 
-function moverSprite(algo, ga, t, c) {//recibe un sprite
+function moverSprite(algo, ga, t) {//recibe un sprite
   algo.on('pointerout', function (pointer) {//se activa cuando se pasa eñ ratpn por  el sprite algo 
     Imagetu = t;
     if (pointer.isDown & !sw) {
@@ -178,7 +177,7 @@ function moverSprite(algo, ga, t, c) {//recibe un sprite
 
       console.log(tl, Imagetu);
 
-     // tlText = ga.add.text(pointer.x, pointer.y, '' + c, { fontSize: '16px', fill: '#0000FF' });
+    //  tlText = ga.add.text(pointer.x, pointer.y, '' + c, { fontSize: '16px', fill: '#0000FF' });
 
       sw = false;
     }
@@ -208,20 +207,15 @@ function cargaInicial(game) {
     swsocket = true;
     tank1.x = ct1; tank2.x = ct2; clr.setText(cl); cud.setText(cu);
     cd1.setText(c1); cd2.setText(c2); cd3.setText(c3); cd4.setText(c4);
-    counter = 0;
+
     s1 = c1; s2 = c2; s3 = c3; s4 = c4; sl = cl; su = cu; st1 = ct1; st2 = ct2;
 
     IDe = game.socket.id;
-    
-    if (per != null) {
-      console.log(per);
-      per.destroy();
-    }
+
   })
-  console.log(s1);
   self = game;
   game.socket.on('otroMovio', function (tube, ID, pls) {
-
+ 
     if (ID == self.socket.id) {
       console.log(".iguales");
     } else {
@@ -240,12 +234,12 @@ function cargaInicial(game) {
   tank2 = groupTank.create(0, 438, 'Tank');
 
   //las guardamos en variables globales para luego poder utilizarlas al momento de construir el grafo
-  clr = game.add.text(880, 93, '' + null, { fontSize: '16px', fill: '#0000FF' });
-  cud = game.add.text(880, 173, '' + null, { fontSize: '16px', fill: '#0000FF' });
-  cd1 = game.add.text(873, 263, '' + null, { fontSize: '16px', fill: '#0000FF' });
-  cd2 = game.add.text(873, 343, '' + null, { fontSize: '16px', fill: '#0000FF' });
-  cd3 = game.add.text(885, 423, '' + null, { fontSize: '16px', fill: '#0000FF' });
-  cd4 = game.add.text(885, 523, '' + null, { fontSize: '16px', fill: '#0000FF' });
+  clr = game.add.text(880, 93, '', { fontSize: '16px', fill: '#0000FF' });
+  cud = game.add.text(880, 173, '', { fontSize: '16px', fill: '#0000FF' });
+  cd1 = game.add.text(873, 263, '', { fontSize: '16px', fill: '#0000FF' });
+  cd2 = game.add.text(873, 343, '', { fontSize: '16px', fill: '#0000FF' });
+  cd3 = game.add.text(885, 423, '', { fontSize: '16px', fill: '#0000FF' });
+  cd4 = game.add.text(885, 523, '', { fontSize: '16px', fill: '#0000FF' });
 
 
   groupTank.enableBody = true;
@@ -254,7 +248,7 @@ function cargaInicial(game) {
   //texto de los jugadores
   game.add.text(16, 530, 'Jugador 1', { fontSize: '32px', fill: '#0000FF' });
   game.add.text(570, 530, 'Jugador 2', { fontSize: '32px', fill: '#FF0000' });
-
+  //text = this.add.text(800, 16, 'Time 00:' + counter, { font: "32px Arial", fill: "#ffffff", align: "center" });
 
   ;
 
